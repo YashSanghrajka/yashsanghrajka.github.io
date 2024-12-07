83ed06c03fdbfbc91f533a50c336f0368d27d0ca
Yash Sanghrajka <sanghrajka.yash@gmail.com>
2024-12-07 15:26:55 -0800

    Commit Test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const headerContent = (
+    <>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload. This is a test.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React, this is a test for cursor.io
+      </a>
+    </>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. This is a test.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {headerContent}
       </header>
     </div>
   );
-}
+};
 
 export default App;
